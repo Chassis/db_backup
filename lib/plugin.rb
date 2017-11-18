853e50d302b44bfcf754e83758789b2d21a71f5b
Bronson Quick <bronson@bronsonquick.com.au>
2017-11-18 12:02:51 +1000

    Add a trigger for vagrant halt

--- lib/plugin.rb
+++ lib/plugin.rb
@@ -13,5 +13,15 @@ module DBBackup
 
 			hook.prepend builder
 		end
+
+		action_hook(:trigger, :machine_action_halt ) do |hook|
+			builder = Builder.new.tap do |b|
+				b.use Builtin::Call, BackupConfirm do |env, b2|
+					DBBackup.backup(env[:machine]) if env[:result]
+				end
+			end
+
+			hook.prepend builder
+		end
 	end
 end
