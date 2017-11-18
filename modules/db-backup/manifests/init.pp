ff6d788126fddd6c57e1c6965d545e522cf4df5a
Bronson Quick <bronson@bronsonquick.com.au>
2017-11-18 13:05:26 +1000

    Fix the top-scope variable

--- modules/db-backup/manifests/init.pp
+++ modules/db-backup/manifests/init.pp
@@ -7,7 +7,7 @@ class db-backup (
 	exec { "${name}-import":
 		command     => "/usr/bin/mysql ${name} < ${file}",
 		logoutput   => true,
-		environment => "HOME=${root_home}",
+		environment => "HOME=${::root_home}",
 		onlyif      => "/usr/bin/test -f ${file}",
 		unless      => "/usr/bin/mysql -e 'DESCRIBE sz_posts' ${name} > /dev/null",
 		require     => Mysql::Db[$name],
